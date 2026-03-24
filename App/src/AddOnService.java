8a243a8bd9f51e64c8c7897b2e48ef8e7e6a1ffd
AnjanShrestha77 <anjanshrestha098@gmail.com>
2026-03-24 16:11:41 +0530

    Use Case 8:Booking History and Reporting

--- App/src/AddOnService.java
+++ App/src/AddOnService.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
+/**
+ * ======================================
+ * AddOnService
+ * ======================================
+ *
+ * this class represent a optional service which can be added to a conformed reservation
+ *
+ * provides getter method for service name and service cost
+ *
+ * @version 7.0
+ */
 public class AddOnService {
+    private String serviceName;
+    private double cost;
+
+    public  AddOnService(String serviceName, double cost){
+        this.serviceName=serviceName;
+        this.cost=cost;
+
+    }
+
+    public String getServiceName(){
+        return serviceName;
+    }
+    public double getCost(){
+        return cost;
+    }
+
 }
